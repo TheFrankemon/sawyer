b0b53a401a95c05bd4a2fd48c872f8c3bc062c34
Allan Leon <aleon.107@outlook.com>
2016-05-26 02:01:30 -0400

    Created MouseRotate script

---
+++ Assets/Scripts/MouseRotate.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseRotate : MonoBehaviour {
+
+	private float rotationSpeed = 10.0F;
+	private float lerpSpeed = 1.0F;
+	
+	private Vector3 theSpeed;
+	private Vector3 avgSpeed;
+	private bool isDragging = false;
+	private Vector3 targetSpeedX;
+	
+	void OnMouseDown() {
+		
+		isDragging = true;
+	}
+	
+	void Update() {
+		
+		if (Input.GetMouseButton(0) && isDragging) {
+			theSpeed = new Vector3(-Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0.0F);
+			avgSpeed = Vector3.Lerp(avgSpeed, theSpeed, Time.deltaTime * 5);
+		} else {
+			if (isDragging) {
+				theSpeed = avgSpeed;
+				isDragging = false;
+			}
+			float i = Time.deltaTime * lerpSpeed;
+			theSpeed = Vector3.Lerp(theSpeed, Vector3.zero, i);
+		}
+		
+		transform.Rotate(Camera.main.transform.up * theSpeed.x * rotationSpeed, Space.World);
+		transform.Rotate(Camera.main.transform.right * theSpeed.y * rotationSpeed, Space.World);
+	}
+}

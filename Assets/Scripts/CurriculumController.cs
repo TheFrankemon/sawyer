c41f215ca1157085bd714bad1600e69644af8d76
Allan Leon <aleon.107@outlook.com>
2016-07-14 04:24:19 -0400

    Implemented Curriculum on desk controller

---
+++ Assets/Scripts/CurriculumController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CurriculumController : MonoBehaviour {
+
+	public string image;
+
+	private Texture2D imgFront;
+	private Texture2D imgBack;
+	private CurriculumMouseRotate curriculumRotateController;
+
+	// Use this for initialization
+	void Start () {
+		imgFront = Resources.Load<Texture2D>("Image/Curriculum/" + image + " front");
+		imgBack = Resources.Load<Texture2D>("Image/Curriculum/" + image + " back");
+		//GetComponent<MeshRenderer> ().material.mainTexture = img;
+		MeshRenderer[] faces = GetComponentsInChildren<MeshRenderer>();
+		faces [0].material.mainTexture = imgBack;
+		faces [2].material.mainTexture = imgFront;
+
+		curriculumRotateController = GameObject.Find ("Curriculum").GetComponent<CurriculumMouseRotate> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnMouseDown() {
+		curriculumRotateController.Show(image);
+	}
+}

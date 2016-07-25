97a53e3ba1324877a84b1fb72177396acd37be41
Allan Leon <aleon.107@outlook.com>
2016-07-25 03:29:07 -0400

    Improved loading screen, added messages

---
+++ Assets/Scripts/Loading/LoadingMessageFactory.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LoadingMessageFactory : MonoBehaviour {
+
+	public GameObject message;
+	public float spawnTime = 5f;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("createLoadingMessage", spawnTime, spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void createLoadingMessage() {
+		//GameObject newMessage = message;
+		//Debug.Log (message);
+		//newMessage.GetComponentInChildren<Text> ().text = "Holiiiiiiiiiiiiiiiiiiiiiiiii";
+		//Sprite newSprite = Resources.Load<Sprite> ("UI/Triangle1");
+		//Debug.Log (newSprite);
+		//newMessage.GetComponentInChildren<Image> ().sprite = Resources.Load<Sprite> ("UI/Triangle1");
+		Instantiate (message, getRandomPosition(), getRandomRotation());
+	}
+
+	Vector3 getRandomPosition() {
+		Vector3 pos = new Vector3 (Random.Range (0f, Screen.width - 500), Random.Range (50f, Screen.height), 0);
+		return pos;
+	}
+
+	Quaternion getRandomRotation() {
+		Quaternion rot = Quaternion.Euler (0, 0, Random.Range (-60f, 0f));
+		return rot;
+	}
+}
---
+++ Assets/Scripts/Loading/PressMe.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressMe : MonoBehaviour {
+
+	public float maxRotation = 7f;
+	public float timeToWait = 100;
+	public int numberOfRepetitions = 5;
+	public float speed = 10f;
+
+	float dRot = 1f;
+	int repetitions = 0;
+	float currentRotation = 0f;
+	bool isWaiting = false;
+	int timeWaited = 0;
+
+	// Use this for initialization
+	void Start () {
+		//InvokeRepeating ("shake", repTime, repTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isWaiting) {
+			if (repetitions <= numberOfRepetitions) {
+				shake();
+			} else {
+				if (center()) {
+					isWaiting = true;
+				}
+			}
+		} else {
+			timeWaited++;
+			if (timeWaited >= timeToWait) {
+				timeWaited = 0;
+				isWaiting = false;
+			}
+		}
+	}
+
+	void shake() {
+		/*currentRotation += dRot;
+		transform.rotation = Quaternion.Euler(0, 0, currentRotation);
+
+		if (Mathf.Abs(currentRotation) > maxRotation) {
+			dRot *= -1;
+			repetitions++;
+		}*/
+		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, maxRotation), speed * Time.deltaTime);
+		if (Mathf.Abs(maxRotation - transform.rotation.eulerAngles.z) < 2) {
+			repetitions++;
+			maxRotation = 360 - maxRotation;
+		}
+	}
+
+	bool center() {
+		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, 0), speed * Time.deltaTime);
+		if (Mathf.Abs(0 - transform.rotation.eulerAngles.z) < 0.1) {
+			repetitions = 0;
+			currentRotation = 0f;
+			transform.rotation = Quaternion.Euler(0, 0, currentRotation);
+			return true;
+		}
+		return false;
+	}
+}

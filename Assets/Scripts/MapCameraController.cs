beb9947fae6ea1cb8ebfb599558604c258a6d040
Allan Leon <aleon.107@outlook.com>
2016-07-29 02:26:17 -0400

    Implemented map expansion

---
+++ Assets/Scripts/MapCameraController.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityStandardAssets.ImageEffects;
+
+public class MapCameraController : MonoBehaviour {
+
+	public Transform target;
+	public int miniFOV = 40;
+	public int fullFOV = 170;
+
+	private Camera cam;
+	private Rect miniRect;
+	private Rect fullRect;
+	private bool fullMap = false;
+	private Quaternion fullMapRotation = Quaternion.Euler(90, 0, 0);
+	private FirstPersonController fpsController;
+	private Vector3 defaultPosition;
+	private BlurOptimized cameraBlur;
+	//private GameObject mapBackground;
+//	private Vector3 fullMapPosition = new Vector3(250
+
+	void Start() {
+		cam = gameObject.GetComponent<Camera> ();
+		fpsController = GameObject.FindGameObjectWithTag ("Player").GetComponent<FirstPersonController>();
+		cameraBlur = GameObject.FindGameObjectWithTag ("Player").GetComponentInChildren<BlurOptimized>();
+		//mapBackground = GameObject.Find ("MapBackground");
+		defaultPosition = new Vector3 (target.position.x, transform.position.y, target.position.z);
+		float width = 0.15f;
+		float height = width * 2;
+		miniRect = new Rect (1 - width, 1 - height, width, height);
+		fullRect = new Rect (-0.1f, 0, 1, 1);
+		//cam.rect = new Rect (Screen.width - width, Screen.height - height, width, height);
+		displayMiniMap ();
+		//cameraBlur.enabled = false;
+		//mapBackground.SetActive (false);
+		//cam.rect = new Rect (0,0, width, width);
+
+		//Debug.Log (width);
+	}
+
+	void Update() {
+		if (Input.GetKeyDown ("m")) {
+			fullMap = !fullMap;
+
+			if (fullMap) {
+				displayFullMap();
+				fpsController.enabled = false;
+				//mapBackground.SetActive(true);
+				cameraBlur.enabled = true;
+			} else {
+				displayMiniMap();
+				fpsController.enabled = true;
+				cameraBlur.enabled = false;
+				//mapBackground.SetActive(false);
+			}
+		}
+	}
+
+	void displayFullMap() {
+		cam.rect = fullRect;
+		cam.orthographicSize = fullFOV;
+	}
+
+	void displayMiniMap() {
+		cam.rect = miniRect;
+		cam.orthographicSize = miniFOV;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		if (!fullMap) {
+			transform.position = new Vector3 (target.position.x, transform.position.y, target.position.z);
+			transform.rotation = Quaternion.Euler (transform.rotation.eulerAngles.x, target.rotation.eulerAngles.y, target.rotation.eulerAngles.z);
+		} else {
+			transform.rotation = fullMapRotation;
+			transform.position = defaultPosition;
+		}
+	}
+}

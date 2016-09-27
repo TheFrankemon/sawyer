d060a6fe4d3a4d0a1e2c5ed36168d65376ba49b1
Allan Leon <aleon.107@outlook.com>
2016-09-27 19:48:14 -0400

    Created TotemIconGoTo script

---
+++ Assets/Scripts/TotemIconGoTo.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TotemIconGoTo : MonoBehaviour {
+
+	public MapCameraController mapController;
+	public GameObject target;
+
+	void OnMouseDown() {
+		mapController.displayFullMap ();
+
+	}
+}

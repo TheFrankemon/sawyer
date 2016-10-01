92ce5c5ef1e71575fe5b8fb2d921cbd9cb24781f
Allan Leon <aleon.107@outlook.com>
2016-10-01 02:40:59 -0400

    Added and implemented all the totem icons

--- Assets/Scripts/TotemIconGoTo.cs
+++ Assets/Scripts/TotemIconGoTo.cs
@@ -11,28 +11,28 @@ public class TotemIconGoTo : MonoBehaviour {
 	private Material[][] defaultMaterials;
 	private bool iconIsActive = false;
 
-	void Start() {
+	/*void Start() {
 		defaultMaterials = new Material[targets.Length][];
 		for (int i = 0; i < targets.Length; i++) {
 			defaultMaterials[i] = targets[i].GetComponent<MeshRenderer>().materials;
 		}
 
 		//Debug.Log ("Materials = " + defaultMaterials.Length);
-	}
+	}*/
 
 	void OnMouseDown() {
-		foreach (GameObject target in targets) {
+		/*foreach (GameObject target in targets) {
 			target.GetComponent<MeshRenderer> ().materials = new Material[] {highlighter};
-		}
+		}*/
 		InvokeRepeating ("BlinkTargetIcons", 0, 0.5f);
 		mapController.displayFullMap ();
 	}
 
 	void Update() {
 		if (Input.GetKeyDown ("escape") || Input.GetKeyDown("m")) {
-			for (int i = 0; i < targets.Length; i++) {
+			/*for (int i = 0; i < targets.Length; i++) {
 				targets[i].GetComponent<MeshRenderer>().materials = defaultMaterials[i];
-			}
+			}*/
 			CancelInvoke();
 			iconIsActive = false;
 			foreach (GameObject icon in targetIcons) {

a543da074a2a0a63699299e3317e3168e1031489
Allan Leon <aleon.107@outlook.com>
2016-10-04 15:26:29 -0400

    Removed name text input and added fade-out effect to logo

---
+++ Assets/Scripts/FadeImageOut.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FadeImageOut : MonoBehaviour {
+
+	private Image img;
+
+	// Use this for initialization
+	void Start () {
+		img = GetComponent<Image> ();
+		img.CrossFadeAlpha (0, 10f, false);
+	}
+}

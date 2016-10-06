7f9ac5adc58f080f8c3000b6500eec7248d44a46
Allan Leon <aleon.107@outlook.com>
2016-10-06 00:39:16 -0400

    Added credits

---
+++ Assets/Scripts/Loading/CreditsController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsController : MonoBehaviour {
+
+	public GameObject[] buttons;
+
+	public void showCredits() {
+		gameObject.SetActive (true);
+		foreach (GameObject button in buttons) {
+			button.SetActive(false);
+		}
+	}
+
+	public void hideCredits() {
+		gameObject.SetActive (false);
+		foreach (GameObject button in buttons) {
+			button.SetActive(true);
+		}
+	}
+}
